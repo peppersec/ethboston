642f18593d2af92b3e8bb2ebc0fdf302d0fb28e8
Arsenii Pechenkin <arspechen@gmail.com>
2019-09-08 01:57:36 +0300

    add decrypt method

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
 
 from flask import Flask, jsonify, request, abort
-from umbral import config, pre
+from umbral import config, pre, keys, signing
 from umbral.curve import SECP256K1
+import random
 
 config.set_default_curve(SECP256K1)
 
@@ -17,22 +18,38 @@ def encrypt():
     signedtext = request.json['data']
     #signedText
     #0x1206df9aa1d4a0b924f01cc75aa4b2456c4490b77d72f955aa331a59ec8f5863024b94a686d1e4cdd5057f4d01eb6fa2431b5ad8a4041b7ab1f9f55b6f1cc6ac1b
-    #encrypting
-    print(alice_public_key)
-    ciphertext, capsule = pre.encrypt(alice_public_key, plaintext)
+    alice = keys.UmbralPublicKey.from_hex(alice_public_key)
+    bob = keys.UmbralPublicKey.from_hex(bob_public_key)
+    ciphertext, capsule = pre.encrypt(alice, signedtext)
 
-    return jsonify(ciphertext), 200
+    #grants access to Bob
+    
+    kfrags = random.sample(kfrags, 10)      # M - Threshold
+    capsule.set_correctness_keys(delegating=alice, receiving=bob, verifying=alice)
+    cfrags = list()                 # Bob's cfrag collection
+    for kfrag in kfrags:
+        cfrag = pre.reencrypt(kfrag=kfrag, capsule=capsule)
+        cfrags.append(cfrag)
+    kfrags = random.sample(kfrags,10)
 
-@app.route('/encript', methods=['POST'])
-def encript():
+    return jsonify({"encrypted":ciphertext, "capsule":capsule}), 200
+
+@app.route('/decrypt', methods=['POST'])
+def decrypt():
     if not request.json :
         abort(400)
 
-    alice_public_key = request.json['sender']
     bob_public_key = request.json['recipient']
-    data = request.json['data']
+    ciphertext = request.json['data']
+    capsule = request.json['capsule']
 
-    return jsonify({'task': request.json['sender']}), 201
+    bob = keys.UmbralPublicKey.from_hex(bob_public_key)
+
+    cleartext = pre.decrypt(ciphertext=ciphertext,
+        capsule=capsule,
+        decrypting_key=bob)
+
+    return jsonify(cleartext), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
